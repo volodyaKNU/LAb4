0adee168dddd493df2bb0fd37adcff2ba97419cb
volodyaKNU <voloda.m@knu.ua>
2025-10-02 01:22:52 +0300

    Add project files.

---
+++ LAb4/mainLogic/AnimalAnalysis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAb4.mainLogic
+{
+    public class AnimalAnalysis
+    {
+
+        public static int getAverage5YearOldAnimal(List<Animal> animals)
+        {
+            if (animals == null || animals.Count == 0) return 0;
+
+            int count = 0;
+            int sumYears = 0;
+
+            foreach (var animal in animals)
+            {
+                if (animal == null) continue;
+
+                if (animal.Age > 5)
+                {
+                    sumYears += animal.Age;
+                    count++;
+                }
+            }
+
+            return count == 0 ? 0 : (sumYears / count);
+        }
+    }
+}
---
+++ LAb4/mainLogic/Dog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAb4.mainLogic
+{
+    public class Dog : Animal
+    {
+        string Breed { get; set; } = string.Empty;
+
+        public Dog(int age)
+        {
+            AnimalType = AnimalT.DOG;
+            AverageLifeDuration = 20;
+            Age = age;
+        }
+
+        public override string Eat() => "dog is eating";
+
+        public override string Sleep() => "dog is sleeping";
+
+        public override string getIDData()
+        {
+            var hasBreed = !string.IsNullOrWhiteSpace(Breed);
+            var breed = hasBreed ? $" порода: {Breed}" : string.Empty;
+
+            return $"Вид: {AnimalType}, Вік: {Age}, Середня тривалість життя: {AverageLifeDuration},{breed}";
+        }
+
+        public string getAgeData()
+        {
+            return !string.IsNullOrWhiteSpace(Breed)
+                ? $"У собаки виявлена така порода: {Breed}"
+                : "Породу собаки не виявлено";
+        }
+    }
+}
